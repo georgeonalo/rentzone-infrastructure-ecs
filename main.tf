e74ff8e482012c0e4946585ed6e72842f2371611
George Onalo <onalotech7@gmail.com>
2023-03-30 18:13:29 +0100

    created terraform files

---
+++ main.tf
@@ -0,0 +1,20 @@
+locals {
+  region       = var.region
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+# create vpc module
+module "vpc" {
+  source                       = "git@github.com:georgeonalo/terraform-modules.git//vpc"
+  region                       = local.region
+  project_name                 = local.project_name
+  environment                  = local.environment
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
---
+++ terraform.tfvars
@@ -0,0 +1,11 @@
+# vpc variables
+region                       = "us-east-1"
+project_name                 = "rentzone"
+environment                  = "dev"
+vpc_cidr                     = "10.0.0.0/16"
+public_subnet_az1_cidr       = "10.0.0.0/24"
+public_subnet_az2_cidr       = "10.0.1.0/24"
+private_app_subnet_az1_cidr  = "10.0.2.0/24"
+private_app_subnet_az2_cidr  = "10.0.3.0/24"
+private_data_subnet_az1_cidr = "10.0.4.0/24"
+private_data_subnet_az2_cidr = "10.0.5.0/24"
